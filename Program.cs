24565c0e2681d089d5739beacdd5a44d72241b6a
Levente <gecsel@kkszki.hu>
2024-10-25 12:31:28 +0200

    Szemelyazonosito project is not finished

--- Program.cs
+++ Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -27,29 +28,43 @@ namespace szemelyazonosito
             Console.WriteLine($"A születési sorszám: {Szulsorszam}");
 
             int szulEv = 1900 + int.Parse(szemszam.Substring(1, 2));
-            if (szemszam[0] == '3' || szemszam[0] == '4') ;
+            if (szemszam[0] == '3' || szemszam[0] == '4');
             {
                 szulEv += 100;
             }
 
             int szuletesnap = ev - szulEv;
             Console.WriteLine($"Ez a személy {szuletesnap} születésnapját ünnepli ebben az évben.");
-
+            Console.WriteLine("");
         }
-            public static void Szemelyazonosito2()
+
+        public static void Szemelyazonosito2()
+        {
+            Console.Write("Adja meg a személyazonosítójának az első 10 jegyét: ");
+            int szulEv1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Adjon meg egy másik személyazonosítónak az első 10 jegyét: ");
+            int szulEv2 = Convert.ToInt32(Console.ReadLine());
+
+            if (szulEv1 > szulEv2)
             {
-                Console.Write("Adjon meg egy másik személyazonosító első 10 jegyét: ");
-                   var szemszam2 = Console.ReadLine();
-
+                Console.WriteLine("Az 1. személy idősebb.");
             }
+            else if (szulEv1 < szulEv2)
+            {
+                Console.WriteLine("A 2. személy idősebb.");
+            }
+            else
+            {
+                Console.WriteLine("A két személy egykorú");
+            }
+               
+            
+        }
 
-         
-
-        
         static void Main(string[] args)
         {
-            Szemelyazonosito();   
-
+            Szemelyazonosito();
+            Szemelyazonosito2();
         }
     }
 }
